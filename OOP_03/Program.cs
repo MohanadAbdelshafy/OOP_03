a9001cc72ae5dc19fbf5763e23e37c76d35a2b63
mohandabdelshafy443-eng <mohandabdelshafy443@gmail.com>
2026-09-20 08:04:25 +0300

    DeliveryHelper

--- OOP_03/Program.cs
+++ OOP_03/Program.cs
@@ -225,6 +225,18 @@
     //    }
     //}
     #endregion
+    #region Part2 6. DeliveryHelper
+    //public static class DeliveryHelper
+    //{
+    //    public static void PrintShipmentDetails(Shipment shipment)
+    //    {
+    //        shipment.PrintShipment();
+    //        if (shipment is StandardShipment) Console.WriteLine("Standard Shipment Printed Successfully.\n");
+    //        else if (shipment is ExpressShipment) Console.WriteLine("Express Shipment Printed Successfully.\n");
+    //        else if (shipment is InternationalShipment) Console.WriteLine("International Shipment Printed Successfully.\n");
+    //    }
+    //}
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
